300a02449ec6a174cb66762d11756c482bd393bf
ArmasterCZ <akrijfuadlaf@seznam.cz>
2018-06-29 21:19:19 +0200

    ?

---
+++ PhoneWriterToAd/PhoneWriterToAd/EventArgsLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace telefonyDoAD
+{
+    class EventArgsLog : EventArgs
+    {
+        public string strLog;
+    }
+}
---
+++ PhoneWriterToAd/PhoneWriterToAd/telephoneUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace telefonyDoAD
+{
+    class telephoneUser
+    {
+        /// <summary>
+        /// Class for store final changes for changes in AD.
+        /// Attributes store AD names of atributes to change. (in same order as attribData)
+        /// </summary>
+
+        public string accountName { get; set; } = "";
+        public List<string> attributes { get; set; } = new List<string>();
+        public List<string> attribData { get; set; } = new List<string>();
+
+        public telephoneUser(string accountName)
+        {
+            this.accountName = accountName;
+        }
+
+        public bool haveChanges()
+        {
+            if (attributes.Count() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
